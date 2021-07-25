2fccd8519466bf1e1cb94b983d10036c8aee153d
ronenk1 <ronenk1@rnd-hub.com>
2021-07-25 17:30:13 +0300

    fix(automation): change permission of copy3

--- pv_executors/executors.py
+++ pv_executors/executors.py
@@ -37,19 +37,21 @@ def copytree(src, dst, symlinks=True, ignore=None):
     """
     This method will make recursive copy process from folder to folder - FileSystem
     """
-    for item in os.listdir(src):
-        try:
-
-            s = os.path.join(src, item)
-            d = os.path.join(dst, item)
-            # shutil.copymode(s, d)
-            if os.path.isdir(s):
-                shutil.copytree(s, d, symlinks, ignore)
-            else:
-
-                shutil.copy2(s, d)
-        except Exception as e:
-            print(str(e))
+    command = f'cp -r {src} {dst}'
+    os.system(command)
+    # for item in os.listdir(src):
+        # try:
+        #
+        #     s = os.path.join(src, item)
+        #     d = os.path.join(dst, item)
+        #     # shutil.copymode(s, d)
+        #     if os.path.isdir(s):
+        #         shutil.copytree(s, d, symlinks, ignore)
+        #     else:
+        #
+        #         shutil.copy2(s, d)
+        # except Exception as e:
+        #     print(str(e))
 
             # s = os.path.join(src, item)
             # d = os.path.join(dst, item)
